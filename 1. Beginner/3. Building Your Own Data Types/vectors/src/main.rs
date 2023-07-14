daf142090ded40975c6298ba26bcdd7732d7039c
Ngoc Hieu Tran <tngochieu@tma.com.vn>
2023-07-14 16:36:11 +0700

    Update testing for struct

--- 1. Beginner/3. Building Your Own Data Types/vectors/src/main.rs
+++ 1. Beginner/3. Building Your Own Data Types/vectors/src/main.rs
@@ -6,8 +6,10 @@ fn main() {
 
     let v2 = vec![1, 2, 3];
 
-    let s = &v[0]; // can panic
-    // let s = v.remove(0);
+    let s = &v[0];
+    // We must have & here.
+    // The vector v is borrowed as immutable by the reference &v[0],
+    // and Rust prevents moving values out of borrowed content to maintain memory safety.
 
     let s = v.get(0);
 
--- 2. Intermediate/1. Polymorphism With Generics & Traits/deriving_traits/src/main.rs
+++ 2. Intermediate/1. Polymorphism With Generics & Traits/deriving_traits/src/main.rs
@@ -3,13 +3,20 @@ struct Point {
     x: i32,
     y: i32,
 }
+struct AnotherPoint {
+    x: i32,
+    y: i32,
+}
 
 fn main() {
     let p1 = Point { x: 3, y: 1 };
     let p2 = Point { x: 3, y: 1 };
+    let another_p2 = AnotherPoint { x: 3, y: 1 };
     let p3 = Point { x: 5, y: 5 };
 
     println!("{:?}", p1);
     println!("{}", p1 == p2);
+    // => The runtime will raise an error due to p1 and another_p2 are different data types
+    // println!("{}", p1 == another_p2);
     println!("{}", p1 == p3);
 }
